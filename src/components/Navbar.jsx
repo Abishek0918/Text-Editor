e108cf4771ff80565242c5c78b11ac28aa3cf958
Abishek0918 <148237019+Abishek0918@users.noreply.github.com>
2024-08-27 12:13:33 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import { FaSun, FaMoon } from 'react-icons/fa';
 const Navbar = ({ mode, toggleMode }) => {
   return (
     <nav className={`navbar ${mode}`}>
-      <h1>My Web App</h1>
+      <h1>OO0</h1>
       <ul className="nav-links">
         <li><Link to="/Translator">Translator</Link></li>
         <li><Link to="/TextTweaker">TextTweaker</Link></li>
         <li><Link to="/vtt">Text-To-Voice</Link></li>
         <li><Link to="/summarizer">Summarizer</Link></li>
-        <li><Link to="/notepad">Notepad</Link></li>
+        <li><Link to="/About">About</Link></li>
       </ul>
       <div className="theme-toggle" onClick={toggleMode}>
         {mode === 'dark' ? <FaSun /> : <FaMoon />}
